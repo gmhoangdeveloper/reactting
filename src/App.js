8809e6fcf4f746bb9495dd35b156f1012eefe745
Giang Minh Hoang <gmhoang.developer@gmail.com>
2024-12-30 21:39:27 +0700

    up v

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Giang minh hoang sơn aws
+          Giang minh hoang sơn okey
         </p>
         <a
           className="App-link"
